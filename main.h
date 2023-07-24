7fb12aaff5357c1ca8fa0366cf57611d982c1a2a
nancie6695 <nanciek004@gmail.com>
2023-07-24 12:03:23 -0700

    Prototypes && Functions

--- main.h
+++ main.h
@@ -2,10 +2,21 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+
+
+#define S_LONG
+#define S_SHORT
+
 
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
+
+
+/* other specifiers */
+int handle_width(const char *format, int *p, va_list list);
+int handle_precision(const char *format, int *p, va_list list);
 
 #endif /* MAIN_H */
 
