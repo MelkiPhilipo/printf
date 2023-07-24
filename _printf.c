965bfc5aeb85cc7544cfaaf546670377cb7fd59a
MelkiPhilipo <melkiphilipo07@gmail.com>
2023-07-24 14:06:25 +0300

    0 commit

--- _printf.c
+++ _printf.c
@@ -1,11 +1,22 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: the number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
-	va_start(args, format);
 	int count = 0;
+	int i;
+
+	va_start(args, format);
+
 	for (int i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
@@ -16,6 +27,7 @@ int _printf(const char *format, ...)
 				case 'c':
 				{
 					char c = va_arg(args, int);
+
 					write(1, &c, 1);
 					count++;
 					break;
@@ -23,6 +35,8 @@ int _printf(const char *format, ...)
 				case 's':
 				{
 					char *str = va_arg(args, char *);
+					int j;
+
 					for (int j = 0; str[j] != '\0'; j++)
 					{
 						write(1, &str[j], 1);
@@ -44,6 +58,8 @@ int _printf(const char *format, ...)
 			count++;
 		}
 	}
+
 	va_end(args);
-	return count;
+
+	return (count);
 }
