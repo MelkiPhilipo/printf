74ad278ad87fe7fa56fe125a1c01211762077883
nancie6695 <nanciek004@gmail.com>
2023-07-25 14:05:42 -0700

    Digits after decimal

--- handle_precision.c
+++ handle_precision.c
@@ -4,7 +4,7 @@
  * handle_precision- calculates the number of digits after the decimal.
  * @format: prints argument of formatted string
  * @p: pointer to interger indicates current position
- * list: list of arguments passed.
+ * @list: list of arguments passed.
  * Return: calculates precisions
  */
 
@@ -16,40 +16,27 @@ int handle_precision(const char *format, int *p, va_list list)
 	if (format[index] != '.')
 	return (precision);
 
-	for (index += 1; format[index] != '\0'; index ++)
-	
+	for (index += 1; format[index] != '\0'; index++)
 	{
-	if (_digit(format[index]))
-	{
-		precision = precision * 10 + (format[index] - '0');
+		if (_isdigit(format[index]))
+		{
+			precision = precision * 10 + (format[index] - '0');
+		}
+		else if (format[index] == (*))
+		{
+			index++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+		{
+			return (-1);
+		}
+
+		if (precision < -1)
+			return (-1);
+		*p = index - 1;
+		return
+			(width);
 	}
-
-	else if (format[index] = (*))
-	{
-		index ++;
-		width = va_arg(list, int);
-		break;
-	}
-
-	else
-	{
-		return (-1);
-	}
-
-	if (width < 0)
-		return (-1);
-	}
-
-	*p = index - 1;
-
-	return
-		(width);
 }
-
-	
-
-        
-
-
-
-
