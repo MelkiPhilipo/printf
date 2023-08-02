57c91e2040f7858b42cbd6dc883b81ff145fd407
MelkiPhilipo <melkiphilipo07@gmail.com>
2023-08-02 15:12:20 +0300

    printf commit

--- _printf.c
+++ _printf.c
@@ -1,111 +1,44 @@
 #include <stdarg.h>
 #include <unistd.h>
-
-/**
- * _printf - format data to standard output
- * @format: pointer to format string
- * @...: variable number of arguments passed
- * Return: Printed characters.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0;
-	int printed_chars = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-int _puts(char *str);
+#include <string.h>
+#include "main.h"
 
 /**
  * _printf - produces output according to a format
- * @format: character string composed of zero or more directives
- *
- * Return: the number of characters printed (excluding the null byte used to)
- * end putput to strings)
+ * @format: character string
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char c;
+	char ch;
 
 	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			printed_chars++;
-		}
-		else
-		{
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
 			format++;
 			switch (*format)
 			{
 				case 'c':
-					c = va_arg(args, int);
-					write(1, &c, 1);
-					count++;
+					ch = (char)va_arg(args, int);
+					write(1, &ch, 1), count++;
 					break;
 				case 's':
-					count += _puts(va_arg(args, char *));
+					count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
 					break;
 				case '%':
-					write(1, "%", 1);
-					count++;
-					break;
-				default:
+					write(1, "%", 1), count++;
 					break;
 			}
 		}
 		else
-		{
-			write(1, format, 1);
-			count++;
-		}
-		format++;
+			write(1, format, 1), count++;
+				format++;
 	}
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer- Prints the contents of the buffer.
- * @buffer: Array of characters
- * @buff_index: Index at which to add next character
- */
-
-void print_buffer(char buffer[], int *buff_index)
-{
-	if (*buff_index > 0)
-		write(1, &buffer[0], *buff_index);
-
-	*buff_index = 0;
-}
-
-/*By nancykibara and melkiphilipo */
 	va_end(args);
-	return (count);
-}
-
-/**
- * _puts - writes a string to the standard output
- * @str: string to write
- *
- * Return: the number of characters written
- */
-int _puts(char *str)
-{
-	int count = 0;
-
-	while (*str)
-	{
-		write(1, str++, 1);
-		count++;
-	}
 	return (count);
 }
